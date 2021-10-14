672981a10bc017beba046ba7d9afcedb5b0ce1c7
boedegoat <bhremada.fka@gmail.com>
2021-10-14 18:30:18 +0700

    delete user api route

--- pages/api/users/[useruid].js
+++
@@ -1,7 +0,0 @@
-import { createRouter } from '../../../lib/api-router'
-import { getUserByUid } from '../../../controllers/users'
-
-export default async function users(req, res) {
-  const router = createRouter(req, res).on('GET', getUserByUid)
-  router.start()
-}
