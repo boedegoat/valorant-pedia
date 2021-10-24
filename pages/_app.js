0bf09125aa5423b57e2643c2ee3c1495bd8b4b4a
boedegoat <bhremada.fka@gmail.com>
2021-10-24 11:47:49 +0700

    add nProgress to display route changing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
 import '../style.css'
 import { Provider } from 'next-auth/client'
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    nProgress.configure({ showSpinner: false })
+
+    function handleStart() {
+      nProgress.start()
+    }
+
+    function handleStop() {
+      nProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
