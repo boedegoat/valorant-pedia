e73039dbbc427318cfb4402124e584154c375d0c
boedegoat <bhremada.fka@gmail.com>
2021-10-24 13:23:38 +0700

    delete unused api route

--- pages/api/agents/[agentname].js
+++
@@ -1,7 +0,0 @@
-import { createRouter } from '../../../lib/api-router'
-import { getAgentByName } from '../../../controllers/agents'
-
-export default function AgentName(req, res) {
-  const router = createRouter(req, res).on('GET', getAgentByName)
-  router.start()
-}
