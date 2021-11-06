a62ec5166049691f7d7a8d02d76e87d16493432f
boedegoat <bhremada.fka@gmail.com>
2021-11-06 13:30:48 +0700

    fix some bugs where some stats might be null

--- components/[weaponName]-page/WeaponStats.js
+++ components/[weaponName]-page/WeaponStats.js
@@ -12,14 +12,14 @@ export default function WeaponStats({ weapon }) {
       <div className='flex-grow border px-3 py-2 rounded-md space-y-3'>
         <h2 className='font-bold text-lg'>📊 Stats</h2>
         <ul className='space-y-2'>
-          <Stat stat='🔥 fire rate' value={weapon.weaponStats.fireRate} />
+          <Stat stat='🔥 fire rate' value={weapon.weaponStats?.fireRate} />
           <Stat
             stat='💨 run speed multiplier'
-            value={weapon.weaponStats.runSpeedMultiplier}
+            value={weapon.weaponStats?.runSpeedMultiplier}
           />
           <Stat
             stat='🎯 first bullet accuracy'
-            value={weapon.weaponStats.firstBulletAccuracy}
+            value={weapon.weaponStats?.firstBulletAccuracy}
           />
         </ul>
         <button
@@ -41,29 +41,29 @@ export default function WeaponStats({ weapon }) {
           <ul className='space-y-2'>
             <Stat
               stat='reload time seconds'
-              value={weapon.weaponStats.reloadTimeSeconds}
+              value={weapon.weaponStats?.reloadTimeSeconds}
             />
             <Stat
               stat='shotgun pellet count'
-              value={weapon.weaponStats.shotgunPelletCount}
+              value={weapon.weaponStats?.shotgunPelletCount}
             />
-            <Stat stat='air bursts stats' value={weapon.weaponStats.airBurstStats} />
-            <Stat stat='feature' value={weapon.weaponStats.feature} />
-            <Stat stat='fire mode' value={weapon.weaponStats.fireMode} />
+            <Stat stat='air bursts stats' value={weapon.weaponStats?.airBurstStats} />
+            <Stat stat='feature' value={weapon.weaponStats?.feature} />
+            <Stat stat='fire mode' value={weapon.weaponStats?.fireMode} />
           </ul>
         </Alert>
       </div>
       <div className='flex-grow border px-3 py-2 rounded-md space-y-3'>
         <h2 className='font-bold text-lg'>🔫 ADS Stats</h2>
         <ul className='space-y-2'>
-          <Stat stat='🔥 fire rate' value={weapon.weaponStats.adsStats.fireRate} />
+          <Stat stat='🔥 fire rate' value={weapon.weaponStats?.adsStats?.fireRate} />
           <Stat
             stat='💨 run speed multiplier'
-            value={weapon.weaponStats.adsStats.runSpeedMultiplier}
+            value={weapon.weaponStats?.adsStats?.runSpeedMultiplier}
           />
           <Stat
             stat='🎯 first bullet accuracy'
-            value={weapon.weaponStats.adsStats.firstBulletAccuracy}
+            value={weapon.weaponStats?.adsStats?.firstBulletAccuracy}
           />
         </ul>
         <button
@@ -83,10 +83,10 @@ export default function WeaponStats({ weapon }) {
             </button>
           </Alert.Title>
           <ul className='space-y-2'>
-            <Stat stat='burst count' value={weapon.weaponStats.adsStats.burstCount} />
+            <Stat stat='burst count' value={weapon.weaponStats?.adsStats?.burstCount} />
             <Stat
               stat='zoom multiplier'
-              value={weapon.weaponStats.adsStats.zoomMultiplier}
+              value={weapon.weaponStats?.adsStats?.zoomMultiplier}
             />
           </ul>
         </Alert>
